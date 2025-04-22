6c9d064b0fc89d655a91300d9bcee4a07a65a7b5
Sebastian Nieto <snrnieto1999@hotmail.com>
2025-04-21 21:19:07 -0500

    Patron factory

--- 01-creacionales/02-factory-method.ts
+++ 01-creacionales/02-factory-method.ts
@@ -12,3 +12,79 @@
  * https://refactoring.guru/es/design-patterns/factory-method
  *
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+interface Hamburger {
+  prepare(): void;
+}
+
+class ChickenHamburger implements Hamburger {
+  prepare(): void {
+    console.log("Preparando una hamburguesa de %cpollo", COLORS.yellow);
+  }
+}
+
+class BeefHamburger implements Hamburger {
+  prepare(): void {
+    console.log("Preparando una hamburguesa de %cres", COLORS.brown);
+  }
+}
+
+class BeanHamburger implements Hamburger {
+  prepare(): void {
+    console.log("Preparando una hamburguesa de %cfrijoles", COLORS.green);
+  }
+}
+abstract class Restaurant {
+  protected abstract createHamburger(): Hamburger;
+
+  orderHamburger(): void {
+    const hamburger = this.createHamburger();
+    hamburger.prepare();
+  }
+}
+
+class ChickenRestaurant extends Restaurant {
+  override createHamburger(): Hamburger {
+    return new ChickenHamburger();
+  }
+}
+
+class BeefRestaurant extends Restaurant {
+  override createHamburger(): Hamburger {
+    return new BeefHamburger();
+  }
+}
+
+class BeanRestaurant extends Restaurant {
+  override createHamburger(): Hamburger {
+    return new BeanHamburger();
+  }
+}
+
+function main() {
+  let restaurant: Restaurant;
+
+  const burgerType = prompt(
+    "¿Qué tipo de hamburguesa quieres? (chicken/beef/bean): "
+  );
+
+  switch (burgerType) {
+    case "chicken":
+      restaurant = new ChickenRestaurant();
+      break;
+    case "beef":
+      restaurant = new BeefRestaurant();
+      break;
+    case "bean":
+      restaurant = new BeanRestaurant();
+      break;
+    default:
+      throw new Error("Tipo de hamburguesa no válida");
+  }
+
+  restaurant.orderHamburger();
+}
+
+main();
--- 01-creacionales/02.2-factory-method.ts
+++ 01-creacionales/02.2-factory-method.ts
@@ -24,7 +24,7 @@
       el prompt para seleccionar el tipo de reporte.
  */
 
-import { COLORS } from '../helpers/colors.ts';
+import { COLORS } from "../helpers/colors.ts";
 
 // 1. Definir la interfaz Report
 interface Report {
@@ -37,17 +37,24 @@ interface Report {
 class SalesReport implements Report {
   // TODO: implementar el método e imprimir en consola:
   // 'Generando reporte de ventas...'
+
+  generate(): void {
+    console.log("Generando reporte de ventas...");
+  }
 }
 
 class InventoryReport implements Report {
   // TODO: implementar el método e imprimir en consola:
   // 'Generando reporte de inventario...'
+  generate(): void {
+    console.log("Generando reporte de inventario...");
+  }
 }
 
 // 3. Clase Base ReportFactory con el Método Factory
 
 abstract class ReportFactory {
-  abstract createReport(): Report;
+  protected abstract createReport(): Report;
 
   generateReport(): void {
     const report = this.createReport();
@@ -59,13 +66,13 @@ abstract class ReportFactory {
 
 class SalesReportFactory extends ReportFactory {
   createReport(): Report {
-    throw new Error('Method not implemented.');
+    return new SalesReport();
   }
 }
 
 class InventoryReportFactory extends ReportFactory {
   createReport(): Report {
-    throw new Error('Method not implemented.');
+    return new InventoryReport();
   }
 }
 
@@ -74,12 +81,9 @@ class InventoryReportFactory extends ReportFactory {
 function main() {
   let reportFactory: ReportFactory;
 
-  const reportType = prompt(
-    '¿Qué tipo de reporte deseas? %c(sales/inventory)',
-    COLORS.red
-  );
+  const reportType = prompt("¿Qué tipo de reporte deseas? (sales/inventory):");
 
-  if (reportType === 'sales') {
+  if (reportType === "sales") {
     reportFactory = new SalesReportFactory();
   } else {
     reportFactory = new InventoryReportFactory();
